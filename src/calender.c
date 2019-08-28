c449823237e191e8e24877b9a1fe548847bb0597
Andy <andy992git@gmail.com>
2019-08-27 20:16:11 -0700

    Added some exception handling as well as a gitignore

--- src/calender.c
+++ src/calender.c
@@ -1,33 +1,51 @@
 #include <stdio.h>
+#include <ctype.h>
+#include "../include/header.h"
 
 int main(){
-	int input;
+
+	char input[10];
+	char *p_input;
+	int option;
+
 	do{
 	printf("1. Find out the Date\n");
 	printf("2. Print out current Month\n");
 	printf("3. Add a note\n");
-	printf("4. Exit\n");
-	scanf("%d",&input);
+	printf("4. Exit");
+	scanf("%s",input);
+	
+	p_input = input;
 
-	switch(input){
+	if( isdigit(input[0]) ){
+	  printf("It is a digit\n");
+	  option = *p_input - '0' ;
+	}
+	else{
+	    continue;
+	}
+	
+	printf("%d",option);
+	switch(option){
 	    case 1:
-		    printf("You have chosen 1");
+		    printf("You have chosen 1\n");
+		    option_1();
 		    break;
 	    case 2:
-		    printf("You have chosen 2");
+		    printf("You have chosen 2\n");
 		    break;
 	    case 3:
-		    printf("You have chosen 3");
+		    printf("You have chosen 3\n");
 		    break;
-	    case 3:
-		    printf("Goodbye");
+	    case 4:
+		    printf("Goodbye\n");
 		    break;
 	    default:
-		    printf("Unknown command");
+		    printf("Unknown command\n");
 		    break;
 
 		}
-	}while(input != 4);
+	}while(option != 4);
 
 	return 0;
 }
---
+++ src/funct.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void option_1(){
+    printf("You have called option1\n");
+
+}
