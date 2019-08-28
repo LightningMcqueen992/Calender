543abfeb2ec5bb0d49b534a9fa1252411260fa0d
Andy <andy992git@gmail.com>
2019-08-27 18:54:59 -0700

    Added a main

--- src/calender.c
+++ src/calender.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(){
-	return 0
+	int input;
+	do{
+	printf("1. Find out the Date\n");
+	printf("2. Print out current Month\n");
+	printf("3. Add a note\n");
+	printf("4. Exit\n");
+	scanf("%d",&input);
+	}while(input != 4);
+
+	return 0;
 }
 
