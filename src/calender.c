4db3eadd221b8e88c99d23d3c11d651067ed8373
Andy <andy992git@gmail.com>
2019-08-27 19:04:17 -0700

    Added switch case to main

--- src/calender.c
+++ src/calender.c
@@ -8,6 +8,25 @@ int main(){
 	printf("3. Add a note\n");
 	printf("4. Exit\n");
 	scanf("%d",&input);
+
+	switch(input){
+	    case 1:
+		    printf("You have chosen 1");
+		    break;
+	    case 2:
+		    printf("You have chosen 2");
+		    break;
+	    case 3:
+		    printf("You have chosen 3");
+		    break;
+	    case 3:
+		    printf("Goodbye");
+		    break;
+	    default:
+		    printf("Unknown command");
+		    break;
+
+		}
 	}while(input != 4);
 
 	return 0;
